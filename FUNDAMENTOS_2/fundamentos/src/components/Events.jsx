7ab7e83ddba19d89f220b1a59f6fe318b6f42117
theselegadev <matheus.seleghin7@gmail.com>
2025-06-04 12:16:24 -0300

    Funcoes no evento

--- FUNDAMENTOS_2/fundamentos/src/components/Events.jsx
+++ FUNDAMENTOS_2/fundamentos/src/components/Events.jsx
@@ -11,7 +11,13 @@ const Events = ()=>{
                 <button onClick={handleMyEvent}>Clique em mim!</button>
             </div>
             <div>
-
+                {/* Função no evento */}
+                <button onClick={()=>console.log("Clicou")}>Clique</button>
+                <button onClick={()=>{
+                    if(true){
+                        console.log("Isso aqui não é recomendavel")
+                    }
+                }}>Clique em mim também</button>
             </div>
         </div>
     )
