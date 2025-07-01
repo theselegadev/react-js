b4d20eed8558b84365f0e7c2fbc43cedb6f07227
theselegadev <matheus.seleghin7@gmail.com>
2025-07-01 15:46:27 -0300

    if ternário para renderização condicional

--- AVANÇANDO_3/avancando/src/components/ConditionalRender.jsx
+++ AVANÇANDO_3/avancando/src/components/ConditionalRender.jsx
@@ -2,13 +2,34 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState(false)
+    const [name, setName] = useState("Matheus")
 
+    function changedName(){
+      if(name === "Matheus"){
+        setName("marcos")
+      }else{
+        setName("Matheus")
+      }
+    }
   return (
     <div>
       {/* Renderização condicional */}
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é falso!</p>}
+        {/* If ternário para renderização condicional */}
+        {name == "Matheus" ? (
+          <div>
+            <p>
+              O nome é Matheus
+            </p>
+          </div>
+        ):(
+          <div>
+            <p>O nome não é Matheus</p>
+          </div>
+        ) }
+        <button onClick={changedName}>Mudar Nome</button>
     </div>
   )
 }
