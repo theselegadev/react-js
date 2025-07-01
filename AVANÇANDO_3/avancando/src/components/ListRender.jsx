ffa66f12bc9deb9a46ef54bdd90283dfd77c9a8e
theselegadev <matheus.seleghin7@gmail.com>
2025-07-01 14:33:45 -0300

    Utilizando a propriedade key

--- AVANÇANDO_3/avancando/src/components/ListRender.jsx
+++ AVANÇANDO_3/avancando/src/components/ListRender.jsx
@@ -2,11 +2,35 @@ import { useState } from "react"
 
 const ListRender = () => {
     const [list] = useState(["Matheus","Gabriel","Josué"])
+    const [users] = useState([
+    {
+        id: 1,
+        firstName: "Matheus",
+        lastName: "Seleghin"
+    },
+    {
+        id: 2,
+        firstName: "Gabriel",
+        lastName: "Seleghin"
+    },
+    {
+        id: 3,
+        firstName: "Lucas",
+        lastName: "Seleghin"
+    }
+])
   return (
     <div>
         <ul>
-            {list.map((item)=>(
-                <li>{item}</li>
+            {/* Utilizando o index do map com key */}
+            {list.map((item, index)=>(
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {/* Utilizando o id do objeto como key*/}
+            {users.map((item)=>(
+                <li key={item.id}>{item.firstName + " " + item.lastName}</li>
             ))}
         </ul>
     </div>
