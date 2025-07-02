46d12713db1bffb5d1ea64eb57f738bd4658a6f8
theselegadev <matheus.seleghin7@gmail.com>
2025-07-02 12:32:48 -0300

    Renderização em lista de componentes

--- AVANÇANDO_3/avancando/src/App.jsx
+++ AVANÇANDO_3/avancando/src/App.jsx
@@ -9,6 +9,11 @@ import CarDetails from './components/CarDetails'
 
 function App() {
   const [name, setName] = useState("Matheus")
+  const [cars] = useState([
+    {id: 1, brand: "Ferrari", color: "vermelho", novo: true, km: 0},
+    {id: 2, brand: "Lamborghini", color: "amarelo", novo: true, km: 0},
+    {id: 3, brand: "Chevrolet", color: "preto", novo: false, km: 2000}
+  ])
 
 
   return (
@@ -34,6 +39,10 @@ function App() {
       {/* Reaproveitamento */}
       <CarDetails brand="Ford" km={300} color="Azul" novo={false}/>
       <CarDetails brand="Citroen" km={1000} color="Preto" novo={true} />
+      {/* Renderização de lista em componente */}
+      {cars.map((car)=>(
+        <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} novo={car.novo}/>
+      ))}
     </>
   )
 }
