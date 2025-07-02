154a49edb3f59be6fff13aa664d493a1d27afbbf
theselegadev <matheus.seleghin7@gmail.com>
2025-07-02 15:35:17 -0300

    prop children

--- AVANÇANDO_3/avancando/src/App.jsx
+++ AVANÇANDO_3/avancando/src/App.jsx
@@ -7,6 +7,7 @@ import ConditionalRender from './components/ConditionalRender'
 import ShowUserName from './components/ShowUserName'
 import CarDetails from './components/CarDetails'
 import Fragment from './components/Fragment'
+import Container from './components/Container'
 
 function App() {
   const [name, setName] = useState("Matheus")
@@ -24,28 +25,44 @@ function App() {
       <div>
          {/* Imagem em public */}
          <img src="/logo_react.png" alt="" />
-      </div>  
+      </div>
+
       {/* Imagem em assets */}
       <div>
         <img src={logo} alt="" />
       </div>
+
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+
       {/* props */}
       <ShowUserName name={name} />
       <button onClick={()=>(setName("Gabriel"))}>Mudar nome:</button>
+
       {/* desestruturação com props */}
       <CarDetails brand="BMW" km={12} color="Cinza" novo={true}/>
+
       {/* Reaproveitamento */}
       <CarDetails brand="Ford" km={300} color="Azul" novo={false}/>
       <CarDetails brand="Citroen" km={1000} color="Preto" novo={true} />
+
       {/* Renderização de lista em componente */}
       {cars.map((car)=>(
         <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} novo={car.novo}/>
       ))}
+
       {/* fragment */}
       <Fragment/>
+
+      {/* Children */}
+      <Container myValue="teste">
+        <p>Este é o conteúdo</p>
+      </Container>
+      <Container myValue="teste2">
+        <h3>Testando container</h3>
+        <p>Qualquer coisa</p>
+      </Container>
     </>
   )
 }
