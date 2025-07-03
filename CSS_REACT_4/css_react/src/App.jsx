29c6d8b59c6fa0a90f28c76dc85c82f39bb839c5
theselegadev <matheus.seleghin7@gmail.com>
2025-07-03 15:34:02 -0300

    Css inline

--- CSS_REACT_4/css_react/src/App.jsx
+++ CSS_REACT_4/css_react/src/App.jsx
@@ -4,12 +4,15 @@ import MyComponent from './components/MyComponent'
 function App() {
   return (
     <>
-    {/* Css global */}
+      {/* Css global */}
       <h1>React com css</h1>
 
       {/* Css de component */}
       <MyComponent/>
       <p>Este parágrafo é do App.jsx</p>
+
+      {/* Inline CSS */}
+      <p style={{color: "blue", padding: "25px", borderTop: "5px solid purple"}}>Este elemento foi estilizado de forma inline</p>
     </>
   )
 }
