e2257b9dbdc88612e19804be0aabb3779efe1f28
theselegadev <matheus.seleghin7@gmail.com>
2025-07-03 15:28:48 -0300

    Css de componentes

--- CSS_REACT_4/css_react/src/App.jsx
+++ CSS_REACT_4/css_react/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   return (
     <>
+    {/* Css global */}
       <h1>React com css</h1>
+
+      {/* Css de component */}
+      <MyComponent/>
+      <p>Este parágrafo é do App.jsx</p>
     </>
   )
 }
---
+++ CSS_REACT_4/css_react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>My component</h1>
+        <p>Este é o parágrafo desse componente</p>
+        <p className="my-comp-p">Esse também é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
