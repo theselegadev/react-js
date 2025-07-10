957cd499d2f6def7efb41dfd92a6e9626ef9a535
theselegadev <matheus.seleghin7@gmail.com>
2025-07-10 12:43:11 -0300

    Alteração de state inline

--- FORM_REACT_5/src/components/MyForm.jsx
+++ FORM_REACT_5/src/components/MyForm.jsx
@@ -5,6 +5,8 @@ const MyForm = () => {
     // gerenciamento de dados
     const [name,setName] = useState()
     const [email,setEmail] = useState()
+    // alteração de state inline
+    const [senha,setSenha] = useState()
 
     function handleName(event){
         setName(event.target.value)
@@ -13,7 +15,7 @@ const MyForm = () => {
         setEmail(event.target.value)
     }
 
-    console.log(name,email)
+    console.log(name,email,senha)
   return (
     <div>
         {/* Criação de formulários */}
@@ -24,6 +26,11 @@ const MyForm = () => {
             <label>
                 <span>E-mail</span>
                 <input type="email" name='email' onChange={handleEmail} placeholder='Digite o seu email'/>
+            </label>
+            {/* Alteração de state inline */}
+            <label>
+                <span>Senha</span>
+                <input type="text" placeholder='senha' onChange={(event)=>(setSenha(event.target.value))}/>
             </label>
             <button type='submit'>Enviar</button>
         </form>
