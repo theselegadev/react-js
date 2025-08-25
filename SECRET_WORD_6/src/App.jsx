08475108905ab1930b42c273d39ca70ee20bf2c4
theselegadev <matheus.seleghin7@gmail.com>
2025-08-25 11:08:39 -0300

    começando o projeto secret word no react

---
+++ SECRET_WORD_6/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import StartScreen from './components/StartScreen'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <StartScreen/>
+    </div>
+  )
+}
+
+export default App
