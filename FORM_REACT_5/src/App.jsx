cdfedaad7d55da6d2c6ae047f2d837a59041484b
theselegadev <matheus.seleghin7@gmail.com>
2025-08-25 10:15:56 -0300

    select no react

--- FORM_REACT_5/src/App.jsx
+++ FORM_REACT_5/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h2>Formulários no react js</h2>
-      <MyForm user={{name: "Gabriel", email: "gabriel@email",senha: "1234"}}/>
+      <MyForm user={{name: "Gabriel", email: "gabriel@email",senha: "1234", bio: "Analista de dados", role: "adm"}}/>
     </>
   )
 }
--- FORM_REACT_5/src/components/MyForm.jsx
+++ FORM_REACT_5/src/components/MyForm.jsx
@@ -7,7 +7,8 @@ const MyForm = ({user}) => {
     const [email,setEmail] = useState(user ? user.email : '')
     // alteração de state inline
     const [senha,setSenha] = useState(user ? user.senha : '')
-    const [bio,setBio] = useState("")
+    const [bio,setBio] = useState(user ? user.bio : '')
+    const [role, setRole] = useState(user ? user.role : '')
 
     function handleName(event){
         setName(event.target.value)
@@ -17,7 +18,7 @@ const MyForm = ({user}) => {
     }
     function handleSubmit(event){
         event.preventDefault()
-        console.log("Dados enviados: ", name, email, senha, bio)
+        console.log("Dados enviados: ", name, email, senha, bio, role)
         setName('')
         setEmail('')
         setSenha('')
@@ -40,10 +41,19 @@ const MyForm = ({user}) => {
                 <span>Senha</span>
                 <input type="text" placeholder='senha' onChange={(event)=>(setSenha(event.target.value))} value={senha}/>
             </label>
-            {/* texta area */}
+            {/* textarea */}
             <label>
                 <span>Bio:</span>
                 <textarea placeholder='Biografia do usuário:' onChange={(e)=>setBio(e.target.value)} value={bio}></textarea>
+            </label>
+            {/* select */}
+            <label>
+                <span>Funções:</span>
+                <select name="role" onChange={(e)=>setRole(e.target.value)} value={role}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="adm">Administrador</option>
+                </select>
             </label>
             <button type='submit'>Enviar</button>
         </form>
