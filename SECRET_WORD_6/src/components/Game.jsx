405dc51e5a34b66755bbf04521e2439b3daef880
theselegadev <matheus.seleghin7@gmail.com>
2025-09-07 11:50:28 -0300

    estrutura do componente game

--- SECRET_WORD_6/src/components/Game.jsx
+++ SECRET_WORD_6/src/components/Game.jsx
@@ -3,8 +3,29 @@ import "./Game.css"
 const Game = ({verifyLetter}) => {
   return (
     <div className="game">
-        <h2>Game</h2>
-        <button onClick={verifyLetter}>Passar stage</button>
+        <p className="points">
+          <span>Pontuação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h3>
+          Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blankSquare"></span>
+        </div>
+        <div className="letterContainer">
+          <p>Adivinhe a letra:</p>
+          <form>
+            <input type="text" maxLength={1} name="letter" required/>
+            <button>Jogar!</button>
+          </form>
+          <div className="wrongLettersContainer">
+            <p>Letras já utilizadas:</p>
+            <span>a,</span>
+            <span>b,</span>
+          </div>
+        </div>
     </div>
   )
 }
