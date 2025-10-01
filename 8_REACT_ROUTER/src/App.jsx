313ca62aaabe68c76e8d6563f2facfc46ae6f9b4
theselegadev <matheus.seleghin7@gmail.com>
2025-10-01 12:12:36 -0300

    navegação de routes

--- 8_REACT_ROUTER/src/App.jsx
+++ 8_REACT_ROUTER/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 
 // 1- config react router
-import {BrowserRouter,Routes,Route, Form} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className='App'>
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
