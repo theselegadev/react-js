3308615fc1001f8b4266df0ec4bbfc634d98e285
theselegadev <matheus.seleghin7@gmail.com>
2025-10-12 17:18:20 -0300

    carregando dados dinâmicamente

--- 8_REACT_ROUTER/src/pages/Product.jsx
+++ 8_REACT_ROUTER/src/pages/Product.jsx
@@ -1,11 +1,27 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+import { useEffect, useState } from "react"
 
 const Product = () => {
     const {id} = useParams()
+    const url = `http://localhost:3000/products/${id}`
+    const [data,setData] = useState("")
+
+    useEffect(()=>{
+        const requestApi = async () => {
+            const response = await fetch(url)
+            const responseJson = await response.json()
+            setData(responseJson)
+        }
+
+        requestApi()
+    },[])
 
   return (
     <>
         <p>ID do produto: {id}</p>
+        <p>Nome: {data.name}</p>
+        <p>Preço: {data.price}</p> 
     </>
   )
 }
