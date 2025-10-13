a5fbee77ef8db0962b2bd28f538cf289e3b2c1e4
theselegadev <matheus.seleghin7@gmail.com>
2025-10-13 11:58:59 -0300

    redirect

--- 8_REACT_ROUTER/src/App.jsx
+++ 8_REACT_ROUTER/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 // 1- config react router
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './components/Navbar'
@@ -23,6 +23,7 @@ function App() {
           <Route path='/products/:id' element={<Product/>}/>
           <Route path='*' element={<NotFound/>}/>
           <Route path='/search' element={<Search/>}/>
+          <Route path='/company' element={<Navigate to="/about"/>}/>
         </Routes>
       </BrowserRouter>
     </div>
