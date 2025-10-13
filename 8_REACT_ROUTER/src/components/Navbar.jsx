4ea322eb7c85547ca878a7cc626ed102b90612fb
theselegadev <matheus.seleghin7@gmail.com>
2025-10-13 11:08:00 -0300

    link ativo

--- 8_REACT_ROUTER/src/components/Navbar.jsx
+++ 8_REACT_ROUTER/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
+        <NavLink to="/" className={({isActive})=>(isActive ? "ativo" : "desativo")}>Home</NavLink>
+        <NavLink to="/about" className={({isActive})=>(isActive ? "ativo" : "desativo")}>About</NavLink>
     </nav>
   )
 }
