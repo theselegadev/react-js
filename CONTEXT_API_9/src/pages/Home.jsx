dcd55606dd23fcd26b45b2022197d89d900a6d21
theselegadev <matheus.seleghin7@gmail.com>
2025-11-01 14:59:47 -0300

    criando um hook para o context

--- CONTEXT_API_9/src/pages/Home.jsx
+++ CONTEXT_API_9/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react"
-
-import {CounterContext} from "../context/CounterContext"
 import ChangeCounter from "../components/ChangeCounter"
+import { useCounterContext } from "../Hooks/useCounterContext"
 
 const Home = () => {
-  const {counter} = useContext(CounterContext)
+  // const {counter} = useContext(CounterContext)
+  const {counter} = useCounterContext()
 
   return (
     <div>
